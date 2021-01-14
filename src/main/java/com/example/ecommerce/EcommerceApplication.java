5c4d78f15274df0e39e7709f4f8518500f51ab3b
REKSOFT\kartavtsev <kartavtsev@reksoft.ru>
2021-01-14 12:19:47 +0300

    Product works.

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,7 +1,15 @@
 package com.example.ecommerce;
 
+//import com.example.application.entities.User;
+//import com.example.application.repositories.UserRepository;
+import java.util.stream.Stream;
+
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -9,5 +17,26 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(ProductService productService) {
+		return args -> {
+			productService.save(new Product(1L, "Test product", 10.4, "https://via.placeholder.com/200x100"));
+			// more products
+		};
+	}
+
+	/*
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
+	 */
 
 }
