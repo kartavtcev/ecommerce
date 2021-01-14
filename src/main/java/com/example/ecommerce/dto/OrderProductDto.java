b8b108aac46864824fd6542fe98669f58d6afe7f
REKSOFT\kartavtsev <kartavtsev@reksoft.ru>
2021-01-14 11:53:56 +0300

    Repositories added.

---
+++ src/main/java/com/example/ecommerce/dto/OrderProductDto.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.dto;
+
+import com.example.ecommerce.model.Product;
+
+public class OrderProductDto {
+
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
